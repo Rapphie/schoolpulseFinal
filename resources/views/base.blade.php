3e2c67682a46df0667f7a1f0f0bc6e8c4afd8687
Rapphie <181870325+Rapphie@users.noreply.github.com>
2025-07-23 12:21:57 +0800

    added chart.js

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -462,6 +462,9 @@
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <!-- Custom JS -->
     {{-- <script src="{{ asset('js/scripts.js') }}"></script> --}}
 
