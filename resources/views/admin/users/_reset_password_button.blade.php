be8351250e38cd80e9dde46e55f7917495ffacb0
Rapphie <181870325+Rapphie@users.noreply.github.com>
2026-02-16 10:58:15 +0800

    refactor: Removed unused schedule editing and viewing pages, along with student management views

--- resources/views/admin/users/_reset_password_button.blade.php
+++
@@ -1,2 +0,0 @@
-<button type="submit" class="btn btn-warning btn-sm" onclick="return confirm('Reset password for this user?')">Reset
-    Password</button>
--- resources/views/teacher/enrollment/disabled.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('base')
-
-@section('content')
-    <div class="container">
-        <h1>Enrollment</h1>
-        <p>Enrollment is currently disabled by the administrator.</p>
-    </div>
-@endsection
